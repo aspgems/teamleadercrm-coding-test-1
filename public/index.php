14dd09531f1677eda578b5bc75a90baf34b3459b
eLafo <javierlafora@gmail.com>
2018-05-19 19:16:33 +0200

    adds logger and a post demo route

--- public/index.php
+++ public/index.php
@@ -8,10 +8,29 @@ $config['displayErrorDetails'] = true;
 $config['addContentLengthHeader'] = false;
 
 $app = new \Slim\App(['settings' => $config]);
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-      $name = $args['name'];
-          $response->getBody()->write("Hello, $name");
 
-          return $response;
+$container = $app->getContainer();
+
+$container['logger'] = function($c) {
+  $logger = new \Monolog\Logger('my_logger');
+  $file_handler = new \Monolog\Handler\StreamHandler("../logs/app.log");
+  $logger->pushHandler($file_handler);
+  return $logger;
+};
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+  $name = $args['name'];
+  $this->logger->addInfo("name: $name");
+  $response->getBody()->write("Hello, $name");
+
+  return $response;
+});
+
+$app->post('/post-demo', function (Request $request, Response $response) {
+  $data= implode($request->getParsedBody());
+  $this->logger->addInfo($data);
+  $response->getBody()->write($data);
+
+  return $response;
 });
 $app->run();
